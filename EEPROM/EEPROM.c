7fddca996a60dc14ce03b8cff9af2e99a2e009bc
Abeer Sobhy <87787045+abeersobhy@users.noreply.github.com>
2022-09-25 20:30:23 +0200

    Add files via upload

---
+++ EEPROM/EEPROM.c
@@ -0,0 +1,24 @@
+﻿/*
+ * EEPROM.c
+ *
+ * Created: 24/09/2022 03:12:42 م
+ *  Author: user
+ */ 
+#include "EEPROM.h"
+uint8_t EEPROM_Read(uint16_t address)
+{
+	while(READ_BIT(EECR , EEWE) == 1); /*Make sure that previous writing is complete.*/
+	EEARL = address;
+	EEARH = (address >> 8);
+	EECR |= (1 << EERE); /*Read Enable.*/
+	return EEDR;
+}
+void EEPROM_Write(uint8_t data , uint16_t address)
+{
+	while(READ_BIT(EECR , EEWE) == 1); /*Make sure that previous writing is complete.*/
+	EEARL = address;
+	EEARH = (address >> 8);
+	EEDR = data;
+	EECR |= (1 << EEMWE); /*Master write Enable.*/
+	EECR |= (1 << EEWE); /*Write Enable.*/	
+}
---
+++ EEPROM/EEPROM.h
@@ -0,0 +1,20 @@
+﻿/*
+ * EEPROM.h
+ *
+ * Created: 24/09/2022 03:12:23 م
+ *  Author: user
+ */ 
+
+
+#ifndef EEPROM_H_
+#define EEPROM_H_
+#define F_CPU 1000000UL
+#include <util/delay.h>
+#include "../../Utilities/bit_math.h"
+#include "../../Utilities/register.h"
+
+uint8_t EEPROM_Read(uint16_t address);
+void EEPROM_Write(uint8_t data , uint16_t address);
+
+
+#endif /* EEPROM_H_ */
