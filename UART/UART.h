2e4aaf92b7fe3815a693c5f08352c774a91542d3
Abeer Sobhy <87787045+abeersobhy@users.noreply.github.com>
2022-09-28 17:07:35 +0200

    Update UART.h

--- UART/UART.h
+++ UART/UART.h
@@ -9,14 +9,15 @@
 #ifndef UART_H_
 #define UART_H_
 
-//#include "../../Utilities/std_types.h"
 #include <util/delay.h>
 #include "../../Utilities/bit_math.h"
 #include "../../Utilities/register.h"
 
-void UART_init();
-void UART_transmit(uint8_t data);
-uint8_t UART_receive();
+void UART_Init();
+/*Send byte to the receiver*/
+void UART_SendByte(uint8_t data);
+/*Receive byte from the transmitter.*/
+uint8_t UART_ReceiveByte();
 void UART_transmit_string(uint8_t *data);
 uint8_t* UART_receive_string();
 
