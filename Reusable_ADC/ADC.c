3c3b0a4dce12fbb01cdff69f089202a91cc40239
Abeer Sobhy <87787045+abeersobhy@users.noreply.github.com>
2022-10-28 11:19:50 +0200

    Add files via upload

---
+++ Reusable_ADC/ADC.c
@@ -0,0 +1,120 @@
+﻿/*
+ * ADC.c
+ *
+ * Created: 04/09/2022 08:57:42 م
+ *  Author: user
+ */ 
+#include "../../Utilities/std_types.h"
+#include "../../Utilities/bit_math.h"
+#include "../../Utilities/register.h"
+#include "ADC.h"
+
+ADC_StructConfig structConfig;
+
+void ADC_init()
+{
+	switch(structConfig.referenceSelection)
+	{
+		case ADC_AREFSelection :
+		     /*Nothing to do.*/
+		break;
+		case ADC_AVCCSelection:
+		     ADMUX |= (1 << REFS0); /*AVCC Mode.*/
+		break;
+		case ADC_InternalVoltSelection :
+		     ADMUX |= (1 << REFS0) | (1 << REFS1); /*Internal 2.56 volt.*/
+		break;
+		default:
+		break;   
+	}
+	switch(structConfig.controlEnable)
+	{
+		case ADC_Enable:
+		     ADCSRA |= (1 << ADEN);
+		break;
+		case ADC_Disable : 
+		     /*Nothing to do.*/
+		break;
+		default:
+		break;
+	}	
+	switch(structConfig.controlPrescaler)
+	{
+		case ADC_divisionFactor_2 :
+		     /*Nothing to do.*/
+		break;
+		case ADC_divisionFactor_4 :
+		     ADCSRA |= (1 << ADPS1);
+		break;
+		case ADC_divisionFactor_8 :
+		     ADCSRA |= (1 << ADPS0)| (1 << ADPS1);
+		break;
+		case ADC_divisionFactor_16 :
+		     ADCSRA |= (1 << ADPS2);
+		break;
+		case ADC_divisionFactor_32 :
+		     ADCSRA |= (1 << ADPS0)| (1 << ADPS2);
+		break;
+		case ADC_divisionFactor_64 :
+		     ADCSRA |= (1 << ADPS1)| (1 << ADPS2);
+		break;
+		case ADC_divisionFactor_128 :
+		     ADCSRA |= (1 << ADPS0)| (1 << ADPS1) | (1 << ADPS2);
+		break;
+		default:
+		break;
+	}
+	switch(structConfig.adjustResult)
+	{
+		case ADC_LeftAdjustResult :
+		     ADMUX |= (1 << ADLAR); 
+		break;
+		case ADC_RightAdjustResult :
+		     /*Nothing to do.*/
+		break;
+		default:
+		break;
+	}
+	switch(structConfig.controlInterrupt)
+	{
+		case ADC_InterruptEnable :
+		     ADCSRA |= (1 << ADIE);
+		break;
+		case ADC_InterruptDisable :
+		     /*Nothing to do.*/
+		break;
+		default:
+		break;
+	}
+	switch(structConfig.autoTriggerControl)
+	{
+		case ADC_AutoTriggerEnable :
+		     ADCSRA |= (1 << ADATE);
+		break;
+		case ADC_AutoTriggerDisable :
+		     /*Nothing to do.*/
+		break;
+		default:
+		break; 
+	}
+}
+uint16_t ADC_read(uint8_t channelNumber)
+{
+	uint16_t ADC = 0;
+	ADMUX |= channelNumber; /*Select channel.*/ 
+	ADCSRA |= (1 << ADSC); /*Start conversion.*/
+	while(READ_BIT(ADCSRA , ADIF) == 0); /*Conversion continue.*/
+    if(structConfig.adjustResult == ADC_LeftAdjustResult)
+	{
+		ADC = (ADCL >> 6);
+		ADC |= (((uint16_t)ADCH) << 2);
+	}
+	else if(structConfig.adjustResult == ADC_RightAdjustResult)
+	{
+		ADC |= ADCH;
+		ADC <<= 8;
+		ADC |= ADCL;	
+	}
+	ADCSRA |= (1 << ADIF); /*Clear the ADIF bit.*/
+	return ADC; /*Return the read value.*/
+}
---
+++ Reusable_ADC/ADC.h
@@ -0,0 +1,73 @@
+﻿#ifndef ADC_H_
+#define ADC_H_
+
+/*Enum to Enable and Disable the ADC.*/
+typedef enum
+{
+	ADC_Enable,
+	ADC_Disable,
+}ADC_ControlEnable;
+/*Enum to AutoTrigger Enable and Disable.*/
+typedef enum
+{
+	ADC_AutoTriggerEnable,
+	ADC_AutoTriggerDisable,
+}ADC_AutoTriggerControl;
+/*Enum to Enable and Disable the ADC Interrupt*/
+typedef enum
+{
+	ADC_InterruptEnable,
+	ADC_InterruptDisable,	
+}ADC_ControlInterrupt;
+/*Enum to select the division factor of thr ADC.*/
+typedef enum
+{
+	ADC_divisionFactor_2,
+	ADC_divisionFactor_4,
+	ADC_divisionFactor_8,
+	ADC_divisionFactor_16,
+	ADC_divisionFactor_32,
+	ADC_divisionFactor_64,
+	ADC_divisionFactor_128,
+}ADC_ControlPrescaler;
+/*Enum to select the voltage reference to the ADC.*/
+typedef enum
+{
+	ADC_AREFSelection,
+	ADC_AVCCSelection,
+	ADC_InternalVoltSelection,
+}ADC_referenceSelection;
+/*Enum to select the Adjust result.*/
+typedef enum
+{
+	ADC_LeftAdjustResult,
+	ADC_RightAdjustResult,
+}ADC_AdjustResult;
+
+typedef struct
+{
+	ADC_ControlEnable         controlEnable;
+	ADC_AutoTriggerControl    autoTriggerControl;
+	ADC_ControlInterrupt      controlInterrupt;
+	ADC_ControlPrescaler      controlPrescaler;
+	ADC_referenceSelection    referenceSelection; 	 	
+    ADC_AdjustResult          adjustResult;
+}ADC_StructConfig;
+
+extern ADC_StructConfig structConfig;
+/***************************************************
+* Function name :  ADC_init
+* Purpose :  initialize The ADC using the selected mode
+* Parameters :   (void) No parameters
+* Return value :  void
+***************************************************/
+void ADC_init();
+/***************************************************
+* Function name :  ADC_read
+* Purpose :  read the input value on the selected channel number
+* Parameters :   (uint8_t) channel_number (to read from)
+* Return value : (uint16_t) the input value to the selected channel
+***************************************************/
+uint16_t ADC_read(uint8_t channel_number);
+
+#endif /* ADC_H_ */
