5afebe4863eac0ac60d77bd995875e9547907a7d
Abeer Sobhy <87787045+abeersobhy@users.noreply.github.com>
2022-11-06 13:19:48 +0200

    Add files via upload

---
+++ REUSABLE_EX_INTERRUPT/ex_interrupt.c
@@ -0,0 +1,103 @@
+﻿#include "../../Utilities/std_types.h"
+#include "../../Utilities/bit_math.h"
+#include "../../Utilities/register.h"
+
+#include "ex_interrupt.h"
+
+void (*pfunc)(void) = NULL;
+INT_StructConfig INT;
+
+void EX_INT_Init()
+{
+	SREG |= (1 << I); /*Enable global interrupt.*/
+	switch(INT.pin)
+	{
+		case INT0_PIN:
+		{
+		     switch(INT.level)
+			 {
+				 case INT_WithLow:
+				      /*Nothing to do.*/
+				 break;
+				 case INT_WithRising:
+				      MCUCR |= (1 << ISC00) | (1 << ISC01); /*Interrupt with rising edge.*/ 
+				 break;
+				 case INT_WithFalling:
+				      MCUCR |= (1 << ISC01); /*Interrupt with falling edge.*/
+				 break;
+				 case INT_WithAnyChange:
+				      MCUCR |= (1 << ISC00); /*Interrupt with rising or falling edge.*/ 
+				 break;
+				 default:
+				 break;
+			 }
+			 GICR |= (1 << INT0); /*External interrupt on INT0 enabled.*/ 
+		}
+		break;
+		case INT1_PIN:
+		{
+			 switch(INT.level)
+		     {
+			     case INT_WithLow:
+			     /*Nothing to do.*/
+			     break;
+			     case INT_WithRising:
+			          MCUCR |= (1 << ISC11) | (1 << ISC10); /*Interrupt with rising edge.*/
+			     break;
+			     case INT_WithFalling:
+			          MCUCR |= (1 << ISC11); /*Interrupt with falling edge.*/
+			     break;
+			     case INT_WithAnyChange:
+			          MCUCR |= (1 << ISC10); /*Interrupt with rising or falling edge.*/
+			     break;
+			     default:
+			     break;
+		     }
+			 GICR |= (1 << INT1); /*External interrupt on INT1 enabled.*/ 
+		}
+		break;
+		case INT2_PIN:
+		{
+		     switch(INT.level)
+		     {
+			     case INT_WithRising:
+			          MCUCSR |= (1 << ISC2); /*Interrupt with rising edge.*/
+			     break;
+			     case INT_WithFalling:
+			         /*Nothing to do*/
+			     break;
+			     default:
+			     break;
+		     }
+			 GICR |= (1 << INT2); /*External interrupt on INT2 enabled.*/
+		}
+		break;
+		default:
+		break;
+   }
+}
+void RegisteringFunction(void(*CallBackFunction)(void)) /*Function to implement the pointer to function with the address of the call back function.*/
+{
+	pfunc = CallBackFunction;	
+}
+ISR(EX_INT_0)
+{
+	if(pfunc != NULL)
+	{
+		(*pfunc)();
+	}
+}
+ISR(EX_INT_1)
+{
+	if(pfunc != NULL)
+	{
+		(*pfunc)();
+	}
+}
+ISR(EX_INT_2)
+{
+	if(pfunc != NULL)
+	{
+		(*pfunc)();
+	}
+}
---
+++ REUSABLE_EX_INTERRUPT/ex_interrupt.h
@@ -0,0 +1,35 @@
+﻿#ifndef EX_INTERRUPT_H_
+#define EX_INTERRUPT_H_
+
+#define NULL '\0'
+#define EX_INT_0 __vector_1
+#define EX_INT_1 __vector_2
+#define EX_INT_2 __vector_3
+#define ISR(INT_VECT)void INT_VECT(void)__attribute__((signal , used));\
+        void INT_VECT(void)
+
+typedef enum 
+{
+	INT_WithRising,
+	INT_WithFalling,
+	INT_WithLow,
+	INT_WithAnyChange,
+}INT_Level;
+typedef enum
+{
+	INT0_PIN,
+	INT1_PIN,
+	INT2_PIN,
+}INT_Pin;
+typedef struct
+{
+	INT_Level level;
+	INT_Pin pin;	
+}INT_StructConfig;
+
+extern INT_StructConfig INT; 		
+void EX_INT_Init(); /*Function to initialize the Interrupt*/
+void RegisteringFunction(void(*ptrfunc)(void));  /*Function to initialize the pointer to function in the driver with the address of the pointer to func.*/
+
+
+#endif /* EX_INTERRUPT_H_ */
